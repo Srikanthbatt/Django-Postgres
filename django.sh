e9d9e8a225e359be17ee4093570f9345168cffff
Srikanth Battula <85862735+Srikanthbatt@users.noreply.github.com>
2023-08-28 16:44:38 +0530

    Update django.sh

--- django.sh
+++ django.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 echo "Create migrations"
-python manage.py makemigrations djangoapp
+python3 manage.py makemigrations djangoapp
 echo "=================================="
 
 echo "Migrate"
-python manage.py migrate
+python3 manage.py migrate
 echo "=================================="
 
 echo "Create Superuser"
 DJANGO_SUPERUSER_USERNAME="admin" \
 DJANGO_SUPERUSER_PASSWORD="" \
 DJANGO_SUPERUSER_EMAIL="email@kanis.hk" \
-python manage.py createsuperuser --noinput
+python3 manage.py createsuperuser --noinput
 echo "=================================="
 
 echo "Start server"
-python manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:8000
