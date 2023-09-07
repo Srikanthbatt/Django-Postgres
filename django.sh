6d217b4e9fb3b4adce70e062bd3c5ac78618ed18
Srikanth Battula <85862735+Srikanthbatt@users.noreply.github.com>
2023-09-07 19:38:24 +0530

    Update django.sh

--- django.sh
+++ django.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Create migrations"
-/usr/local/bin/python manage.py makemigrations djangoapp
+python3 manage.py makemigrations djangoapp
 echo "=================================="
 
 echo "Migrate"
